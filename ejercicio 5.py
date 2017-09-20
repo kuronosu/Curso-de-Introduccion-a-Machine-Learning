289cb63f5e63e21447a3759b9de1e880d689f1b3
Andrés Felipe Martínez Salazar <KR0N0S31@users.noreply.github.com>
2017-09-19 23:50:41 -0500

    Ejercicio escrito: Compuerta Logica OR

---
+++ ejercicio 5.py
@@ -0,0 +1,20 @@
+def g(pesos, a, b):
+    salida = (pesos[0] + pesos[1]*a + pesos[2]*b)
+    if salida <= 0:
+        return 0
+    else:
+        return 1
+
+if __name__ == '__main__':
+    a = [0,0,1,1]
+    b = [0,1,0,1]
+    pesos_or = [-10,20,20]
+    pesos_and = [-30,20,20]
+    salida_or = []
+    salida_and = []
+    for i in range(len(a)):
+        salida_or.append(g(pesos_or, a[i], b[i]))
+        salida_and.append(g(pesos_and, a[i], b[i]))
+
+    print('El output de la conpuerta logica or es: {}'.format(salida_or))
+    print('El output de la conpuerta logica and es: {}'.format(salida_and))
